9b868bdcedf27dc6f7800442a0a631422aa8c54a
Paweł Socała <psocala12@gmail.com>
2024-11-18 17:20:29 +0100

    ForkJoinPool update

--- lab_7/src/JavaForkJoin/DivideTask.java
+++ lab_7/src/JavaForkJoin/DivideTask.java
@@ -3,7 +3,7 @@ import java.util.concurrent.RecursiveTask;
 
 public class DivideTask extends RecursiveTask<int[]> {
 
-    private static final int THRESHOLD = 10; // Minimalna wielkość podziału
+    private static final int THRESHOLD = 10;
     private int[] arrayToDivide;
 
     public DivideTask(int[] arrayToDivide) {
@@ -13,10 +13,8 @@ public class DivideTask extends RecursiveTask<int[]> {
     @Override
     protected int[] compute() {
         if (arrayToDivide.length <= THRESHOLD) {
-            // Sortowanie sekwencyjne dla małych tablic
             return sequentialSort(arrayToDivide);
         } else {
-            // Podziel tablicę na pół
             int mid = arrayToDivide.length / 2;
 
             int[] leftPart = new int[mid];
@@ -25,48 +23,44 @@ public class DivideTask extends RecursiveTask<int[]> {
             System.arraycopy(arrayToDivide, 0, leftPart, 0, mid);
             System.arraycopy(arrayToDivide, mid, rightPart, 0, rightPart.length);
 
-            // Utwórz nowe zadania
             DivideTask task1 = new DivideTask(leftPart);
             DivideTask task2 = new DivideTask(rightPart);
 
-            // Uruchom zadania równolegle
             task1.fork();
             task2.fork();
 
-            // Czekaj na wyniki i scal
             int[] sortedLeft = task1.join();
             int[] sortedRight = task2.join();
 
             int[] result = new int[arrayToDivide.length];
-            scal_tab(sortedLeft, sortedRight, result);
+            scalTab(sortedLeft, sortedRight, result);
 
             return result;
         }
     }
 
     private int[] sequentialSort(int[] array) {
-        // Proste sortowanie np. przez wstawianie
         java.util.Arrays.sort(array);
         return array;
     }
 
-    private void scal_tab(int[] tab1, int[] tab2, int[] scal_tab) {
+    private void scalTab(int[] tab1, int[] tab2, int[] scalTab) {
         int i = 0, j = 0, k = 0;
 
         while ((i < tab1.length) && (j < tab2.length)) {
             if (tab1[i] < tab2[j]) {
-                scal_tab[k++] = tab1[i++];
+                scalTab[k++] = tab1[i++];
             } else {
-                scal_tab[k++] = tab2[j++];
+                scalTab[k++] = tab2[j++];
             }
         }
 
         while (i < tab1.length) {
-            scal_tab[k++] = tab1[i++];
+            scalTab[k++] = tab1[i++];
         }
 
         while (j < tab2.length) {
-            scal_tab[k++] = tab2[j++];
+            scalTab[k++] = tab2[j++];
         }
     }
 }
